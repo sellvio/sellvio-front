2baaf43a441a928e630a089e5e63c5a2009327c0
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-09-21 21:14:28 +0400

    Refactor CompanyBasics form layout and styling
    Simplified and improved the layout of the CompanyBasics form by using flexbox for responsive design, making input fields full width, and removing unnecessary wrapper elements. Enhanced overall readability and maintainability of the component.

--- feature/createCampaing/components/primitives/CompanyBasics.tsx
+++ feature/createCampaing/components/primitives/CompanyBasics.tsx
@@ -22,7 +22,7 @@ const CompanyBasics = () => {
   });
 
   return (
-    <div className="max-w-[1222px] w-full bg-transparent mx-auto  rounded-[8px] px-[30px] py-[30px] flex flex-col border ">
+    <div className="max-w-[1222px] w-full bg-transparent mx-auto rounded-[8px] px-[30px] py-[30px] flex flex-col border justify-center">
       <div className="flex flex-col">
         <div className="flex items-center gap-2">
           <Image
@@ -40,79 +40,67 @@ const CompanyBasics = () => {
         </p>
       </div>
 
-      <form className="flex-col gap-[75px] mx-auto">
-        <div className="flex gap-[75px] mt-[26px]">
-          <div>
+      <form className="flex flex-col gap-[37px] mt-[26px] w-full">
+        <div className="flex flex-col lg:flex-row gap-[75px] w-full">
+          <div className="flex-1">
             <h3 className="text-[#000000] font-[700] text-[18px] mb-4">
               კამპანიის სახელი
             </h3>
-            <div>
-              <label className="block mb-1 text-sm font-medium"></label>
-              <input
-                type="text"
-                placeholder="მაგ: საზაფხულო პროდუქტი"
-                {...register("companyName")}
-                className="w-[543px] border border-[#E3E8EF] rounded px-3 py-2 text-[#000000] font-[700] outline-none"
-              />
-              {errors.companyName && (
-                <span className="text-xs text-red-500">
-                  {errors.companyName.message}
-                </span>
-              )}
-            </div>
+            <input
+              type="text"
+              placeholder="მაგ: საზაფხულო პროდუქტი"
+              {...register("companyName")}
+              className="w-full border border-[#E3E8EF] rounded px-3 py-2 text-[#000000] font-[700] outline-none"
+            />
+            {errors.companyName && (
+              <span className="text-xs text-red-500">
+                {errors.companyName.message}
+              </span>
+            )}
           </div>
 
-          <div>
-            <div>
-              <h3 className="font-[700] text-[18px] text-[#000000] mb-4 ">
-                ბიუჯეტი (ლარში)
-              </h3>
-              <div>
-                <label className="block mb-1 text-sm font-medium"></label>
-                <input
-                  type={isOn ? "password" : "text"}
-                  placeholder="₾ 5000"
-                  {...register("budget")}
-                  className="w-[543px] border border-[#E3E8EF] rounded px-3 py-2 text-[#000000] font-[700] outline-none"
-                />
-                {errors.budget && (
-                  <span className="text-xs text-red-500">
-                    {errors.budget.message}
-                  </span>
-                )}
-              </div>
-            </div>
+          <div className="flex-1">
+            <h3 className="font-[700] text-[18px] text-[#000000] mb-4">
+              ბიუჯეტი (ლარში)
+            </h3>
+            <input
+              type={isOn ? "password" : "text"}
+              placeholder="₾ 5000"
+              {...register("budget")}
+              className="w-full border border-[#E3E8EF] rounded px-3 py-2 text-[#000000] font-[700] outline-none"
+            />
+            {errors.budget && (
+              <span className="text-xs text-red-500">
+                {errors.budget.message}
+              </span>
+            )}
 
             <div
-              className="w-full flex items-center gap-3 cursor-pointer mt-3 justify-end "
+              className="w-full flex items-center gap-3 cursor-pointer mt-3 justify-end"
               onClick={toggleHandler}
             >
               <p className="text-[#000000] font-[700]">
                 დამალე ბიუჯეტი შემქნელებისთვის
               </p>
-
               <ToggleSwitch value={isOn} onToggle={toggleHandler} />
             </div>
           </div>
         </div>
 
-        <div className="mt-[37px]">
+        <div className="w-full">
           <h3 className="font-[700] text-[18px] text-[#000000] mb-4">
             კამპანიის აღწერა
           </h3>
-          <div>
-            <label className="block mb-1 text-sm font-medium"></label>
-            <textarea
-              placeholder="აღწერეთ თქვენი კამპანიის მიზნები, სამიზნე აუდიტორია და რას ეძებთ შემქმნელებში..."
-              {...register("companyDesc")}
-              className="w-full border border-[#E3E8EF] rounded px-3 py-2 text-[#000000] min-h-[218px] font-[700] outline-none resize-none"
-            />
-            {errors.companyDesc && (
-              <span className="text-xs text-red-500">
-                {errors.companyDesc.message}
-              </span>
-            )}
-          </div>
+          <textarea
+            placeholder="აღწერეთ თქვენი კამპანიის მიზნები, სამიზნე აუდიტორია და რას ეძებთ შემქმნელებში..."
+            {...register("companyDesc")}
+            className="w-full border border-[#E3E8EF] rounded px-3 py-2 text-[#000000] min-h-[218px] font-[700] outline-none resize-none"
+          />
+          {errors.companyDesc && (
+            <span className="text-xs text-red-500">
+              {errors.companyDesc.message}
+            </span>
+          )}
         </div>
       </form>
     </div>
