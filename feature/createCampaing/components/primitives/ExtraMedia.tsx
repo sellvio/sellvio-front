d1ddb8fbbab9560020289b63845aa90ff98c3eab
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-09-23 16:44:17 +0400

    bugfix/extramedia

--- feature/createCampaing/components/primitives/ExtraMedia.tsx
+++ feature/createCampaing/components/primitives/ExtraMedia.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { UploadFile } from "../../data/data";
 
 const ExtraMedia = () => {
   const [open, setOpen] = useState(false);
@@ -64,61 +63,77 @@ const ExtraMedia = () => {
           )}
         </AnimatePresence>
       </div>
+
       <div className="flex w-full mx-auto gap-5 flex-wrap mt-5">
-        {UploadFile.map((eachElement) => (
+        <div className="w-full m-auto lg:max-w-[570px] flex flex-col gap-5 justify-center items-center border border-dashed pt-[21px] pb-[21px] rounded-[8px]">
           <div
-            key={eachElement.id}
-            className="w-full m-auto lg:max-w-[570px] flex flex-col gap-5 justify-center items-center border border-dashed  pt-[21px] pb-[21px]  rounded-[8px]"
+            className={`w-[160px] ${
+              file && "mb-[20px] "
+            }h-[80px] flex items-center justify-center`}
           >
-            <div
-              className={`w-[160px] ${
-                file && "mb-[20px] "
-              }h-[80px] flex items-center justify-center`}
-            >
-              {file ? (
-                <div className="flex flex-col items-center justify-center gap-2 mt-2">
-                  {file && (
-                    <div className="flex py-[5px] flex-col items-center mt-2">
-                      <Image
-                        src={URL.createObjectURL(file)}
-                        alt="Preview"
-                        width={60}
-                        height={60}
-                        className="object-cover rounded"
-                      />
-                    </div>
-                  )}
-                </div>
-              ) : (
-                <div className="w-full flex-col flex items-center justify-center">
+            {file ? (
+              <div className="flex flex-col items-center justify-center gap-2 mt-2">
+                <div className="flex py-[5px] flex-col items-center mt-2">
                   <Image
-                    src={eachElement.img}
-                    width={40}
-                    height={40}
-                    alt={eachElement.title}
+                    src={URL.createObjectURL(file)}
+                    alt="Preview"
+                    width={60}
+                    height={60}
+                    className="object-cover rounded"
                   />
-                  <p className="text-[var(--campaing-form-paragraphs)] font-[700]">
-                    {eachElement.title}
-                  </p>
                 </div>
-              )}
-            </div>
-
-            <input
-              type="file"
-              id="fileInput"
-              className="hidden"
-              onChange={handleFileChange}
-            />
-
-            <button
-              className="w-[267px] px-2 py-3 rounded-[8px] cursor-pointer bg-[var(--button-bg)] text-[var(--white-color)]"
-              onClick={() => document.getElementById("fileInput")?.click()}
-            >
-              აირჩიე ფაილი
-            </button>
+              </div>
+            ) : (
+              <div className="w-full flex-col flex items-center justify-center">
+                <Image
+                  src="/images/svg/gallery.svg"
+                  width={40}
+                  height={40}
+                  alt="ატვირთე ფაილი"
+                />
+                <p className="text-[var(--campaing-form-paragraphs)] font-[700]">
+                  ატვირთე ფაილი
+                </p>
+              </div>
+            )}
           </div>
-        ))}
+
+          <input
+            type="file"
+            id="fileInput1"
+            className="hidden"
+            onChange={handleFileChange}
+          />
+
+          <button
+            className="w-[267px] px-2 py-3 rounded-[8px] cursor-pointer bg-[var(--button-bg)] text-[var(--white-color)]"
+            onClick={() => document.getElementById("fileInput1")?.click()}
+          >
+            აირჩიე ფაილი
+          </button>
+        </div>
+
+        <div className="w-full m-auto lg:max-w-[570px] gap-5 flex flex-col justify-center items-center border border-dashed  pt-[21px] pb-[21px] rounded-[8px]">
+          <div className="w-[160px] h-[63px] flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center ">
+              <div className="w-full flex-col flex items-center justify-center">
+                <Image
+                  src="/images/svg/upload.svg"
+                  alt="ლინკი"
+                  width={39}
+                  height={39}
+                />
+                <p className="text-[var(--campaing-form-paragraphs)] font-[700]">
+                  ატვირთე ლინკი
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <button className="w-[267px] px-2 py-3 rounded-[8px] cursor-pointer bg-[var(--button-bg)] text-[var(--white-color)]">
+            აირჩიე ლინკი
+          </button>
+        </div>
       </div>
     </div>
   );
