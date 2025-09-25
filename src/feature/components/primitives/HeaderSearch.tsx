c826bbf06f2cff6ef5e8c60f3f430551aa84e83a
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-09-26 00:23:16 +0400

    feature/header-search

--- src/feature/components/primitives/HeaderSearch.tsx
+++ src/feature/components/primitives/HeaderSearch.tsx
@@ -4,18 +4,24 @@ import { useState } from 'react';
 const HeaderSearch = () => {
   const [search, setSearch] = useState(false);
   return (
-    <div className="flex justify-end bg-[var(--header-icon-bg)] border border-[var(--header-icon-border)] rounded-[16px]">
+    <div
+      className={`flex justify-between bg-[var(--header-icon-bg)] ease-in-out transition-all duration-300 ${
+        search
+          ? 'max-w-[485px] w-full min-h-[50px]'
+          : 'max-w-[50px] min-h-[50px] w-full'
+      } border border-[var(--header-icon-border)] rounded-[16px]`}
+    >
       <input
         type="search"
         className={`${
           search
-            ? 'max-w-[485px] min-h-[50px] p-[14px]'
+            ? 'max-w-[433px] w-full min-h-[50px] p-[14px]'
             : 'max-w-[0px] min-h-[0px]'
         } transition-all duration-600 ease-in-out outline-none no-clear`}
       />
       <button
         onClick={() => setSearch(!search)}
-        className="flex justify-center items-center w-[50px] h-[50px]"
+        className="flex justify-center items-center w-[50px] h-[50px] cursor-pointer"
       >
         <Image
           src="/images/svg/search.svg"
