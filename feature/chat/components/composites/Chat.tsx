b43941ff19073f9bea332c13173193d179d9560d
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-12-16 23:26:59 +0400

    feature/changeChatPage

--- feature/chat/components/composites/Chat.tsx
+++ feature/chat/components/composites/Chat.tsx
@@ -4,6 +4,7 @@ import ChatHeader from '../../../components/composites/ChatHeader';
 import ChanelInfo from '../primitives/ChanelInfo';
 import Channels from '../primitives/Channels';
 import GeneralChat from '../primitives/GeneralChat';
+import ChanelInfoSidebar from '../primitives/ChanelInfoSidebar';
 
 const Chat = () => {
   const [chatInfoOpen, setChatInfoOpen] = useState<boolean>(false);
@@ -12,7 +13,12 @@ const Chat = () => {
     <div className="flex flex-col m-auto w-full max-w-[1440px] min-h-screen">
       <ChatHeader />
       <div className="flex rounded-[10px] overflow-hidden">
-        <Channels setChatInfoOpen={setChatInfoOpen} />
+        {chatInfoOpen ? (
+          <ChanelInfoSidebar />
+        ) : (
+          <Channels setChatInfoOpen={setChatInfoOpen} />
+        )}
+
         {chatInfoOpen ? (
           <ChanelInfo setChatInfoOpen={setChatInfoOpen} />
         ) : (
---
+++ feature/chat/components/primitives/ChanelInfoSidebar.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+
+const ChanelInfoSidebar = () => {
+  return (
+    <div className="flex flex-col gap-[10px] bg-[#001541D6] py-[16px] border-[#E0E0E0] border-r w-full max-w-[277px] h-screen">
+      <div className="flex items-center gap-[10px] px-[13px] w-full">
+        <Image
+          src={'/images/chatIcons/svg/hashtag.svg'}
+          alt="# icon"
+          width={16}
+          height={16}
+        />
+        <p className="text-[15px] text-white">საერთო-ჩატი</p>
+      </div>
+      <div className="pl-[13px]">
+        <div className="hover:bg-[#FFFFFF36] px-[8px] py-[6px] rounded-tl-[6px] rounded-bl-[6px] cursor-pointer">
+          <p className="font-semibold text-[15px] text-white">
+            არხის ინფორმაცია
+          </p>
+        </div>
+        <div className="hover:bg-[#FFFFFF36] px-[8px] py-[6px] rounded-tl-[6px] rounded-bl-[6px] cursor-pointer">
+          <p className="font-semibold text-[15px] text-white">
+            მოწვეის გაკეთება
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#FFFFFF75] w-full h-[1px]"></div>
+      <div className="flex items-center gap-[10px] pl-[21px] w-full">
+        <Image
+          src={'/images/chatIcons/svg/deleteSvg.svg'}
+          alt="# icon"
+          width={16}
+          height={16}
+        />
+        <p className="text-[#FF86AC] text-[15px]">არხის წაშლა</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChanelInfoSidebar;
