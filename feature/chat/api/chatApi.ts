076f2ab43bafc2802382c766a4db8cd47bd3f34f
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-12-21 02:57:47 +0400

    feature/dynamic-member-list

--- feature/chat/api/chatApi.ts
+++ feature/chat/api/chatApi.ts
@@ -12,13 +12,27 @@ export const ChatFromCampaing = async (id: number) => {
   return res.json();
 };
 
-export async function addChanel() {
+export const ChatMember = async () => {
+  const token = localStorage.getItem('access_token');
+  const res = await fetch(`${baseUrl}/chat-servers/7/channels/get/members`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    cache: 'no-store',
+  });
+  if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+  return res.json();
+};
+
+export async function addChanel(data) {
   const token = localStorage.getItem('access_token');
   const res = await fetch(`${baseUrl}/chat-servers/7/channels`, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
+    body: JSON.stringify(data),
   });
 
   if (!res.ok) {
--- feature/chat/components/primitives/Member.tsx
+++ feature/chat/components/primitives/Member.tsx
@@ -1,17 +1,22 @@
 'use client';
 import Image from 'next/image';
-import { users } from '../../data/chatData';
+import { useQuery } from '@tanstack/react-query';
+import { ChatMember } from '../../api/chatApi';
 
 const Member = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['member'],
+    queryFn: ChatMember,
+  });
   return (
     <div className="[border-bottom-left-radius:10px] [border-top-left-radius:10px] gap-[14px] bg-[linear-gradient(0deg,rgba(17,24,39,0.42),rgba(17,24,39,0.42)),linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.1))] w-full max-w-[304px] h-full overflow-y-auto">
-      {users.map((eachElement) => (
+      {data?.data.map((eachElement) => (
         <div
-          key={eachElement.id}
+          key={eachElement.user.id}
           className="flex items-center hover:bg-[#FFFFFF36] px-[22px] py-[11px] min-h-[56px] duration-300 ease-in-out cursor-pointer"
         >
           <div className="flex items-center gap-[15px]">
-            <div className="relative bg-[#999999] rounded-full w-[31px] h-[31px]">
+            {/* <div className="relative bg-[#999999] rounded-full w-[31px] h-[31px]">
               <Image
                 src={
                   eachElement.status === 'active'
@@ -23,13 +28,13 @@ const Member = () => {
                 height={8}
                 className="right-[1px] bottom-[1px] absolute"
               />
-            </div>
+            </div> */}
             <div>
               <p className="font-semibold text-[#FFFFFF] text-[15px]">
-                {eachElement.userName}
+                {eachElement.user.email}
               </p>
               <p className="font-semibold text-[12px] text-gray-500">
-                {eachElement.status}
+                {eachElement.role}
               </p>
             </div>
           </div>
